a0f0456058c76350dfd61fff470345ef5ac0a6ed
Name <email@example.ru>
2021-01-09 14:58:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Andrey";
+            byte MyAge = 15;
+            bool HaveIPet = false;
+            double MyShoeSize = 42.7;
+
+         Console.WriteLine("My name is" + MyName);
+         Console.WriteLine("My age is " + MyAge);
+         Console.WriteLine("Do I have pet?" + HaveIPet);
+         Console.WriteLine("My shoe size is" + MyShoeSize);
+
+         Console.ReadKey();
+        }
+    }
+}
